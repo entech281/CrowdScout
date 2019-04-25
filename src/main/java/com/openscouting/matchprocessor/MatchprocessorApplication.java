5137832ee9422c62bba1e5ba88825ff266347359
PLaba <killermonkeychicken@gmail.com>
2019-04-25 15:38:08 -0400

    Started spring boot project

---
+++ src/main/java/com/openscouting/matchprocessor/MatchprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.openscouting.matchprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchprocessorApplication.class, args);
+	}
+
+}
