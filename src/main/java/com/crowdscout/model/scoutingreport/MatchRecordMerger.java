80e1de7601c07bf621d53209853cb966a947ae9d
Peter Labadorf <plaba3.1415@gmail.com>
2019-09-09 18:43:09 -0400

    I'm doing stuff

--- src/main/java/com/crowdscout/model/scoutingreport/MatchRecordMerger.java
+++ src/main/java/com/crowdscout/model/scoutingreport/MatchRecordMerger.java
@@ -6,18 +6,24 @@ import java.util.List;
 
 public class MatchRecordMerger {
 
-    private static double gapscore = -1;
-    //private static double[][] costArray;
+    private static final int acceptableTimeDifference = 6;
+    private static final int acceptableTimeDifferenceSquared = acceptableTimeDifference * acceptableTimeDifference;
+    private static final double gapscore = -1;
 
-
-    static MatchRecord mergeReports(MatchRecord report1, MatchRecord report2){
-        
     
-        int row, col;    // for indexing through array
-        double northwest, north, west;  // (row, col) entry will be max of these
 
-        int xlen = report1.getEvents().size();
-        int ylen = report2.getEvents().size();
+    public static MatchRecord mergeReports(MatchRecord report1, MatchRecord report2){
+        
+        List<Node> nodes = getNodesOnBestPath(report1.getEvents(), report2.getEvents());
+        AlignEventLists(report1.getEvents(), report2.getEvents(), nodes);
+        return new MatchRecord(report1.getEvents());
+    }
+
+    private static List<Node> getNodesOnBestPath(List<ReportedEvent> events1, List<ReportedEvent> events2){
+        int row, col;    // for indexing through array
+
+        int xlen = events1.size();
+        int ylen = events2.size();
 
         Node[][] scoreArray = new Node[xlen][ylen];
         // Fill the top row and left column:
@@ -33,14 +39,14 @@ public class MatchRecordMerger {
         // Now fill in the rest of the array:
         for (row=1; row <= ylen; row++) {
             for (col=1; col <= xlen; col++) {
-                
-                //TODO get cost for northwest, north, and west.
+                double northwest, north, west;
+                northwest = GetAcceptBothScore(events1.get(col), events2.get(row));
                 north = gapscore;
                 west = gapscore;
 
                 Node current = new Node(col, row);
 
-                //current.updateParent(scoreArray[ row - 1 ][ col -1], northwest);
+                current.updateParent(scoreArray[ row - 1 ][ col -1], northwest);
                 current.updateParent(scoreArray[ row - 1 ][ col ], north);
                 current.updateParent(scoreArray[ row ][ col - 1 ], west);
 
@@ -63,6 +69,55 @@ public class MatchRecordMerger {
 
         Collections.reverse(nodesOnBestPath);
 
+        nodesOnBestPath.remove(0);
+
+        return nodesOnBestPath;
+    }
+
+    private static double GetAcceptBothScore(ReportedEvent e1, ReportedEvent e2){
+        return - Math.pow( ( e1.getStartedTime() - e2.getStartedTime()),1)
+                     - Math.pow( e1.getDuration() - e1.getDuration(), 2)
+                     + 2 * acceptableTimeDifferenceSquared;
+    }
+
+    private static void AlignEventLists(List<ReportedEvent> list1, List<ReportedEvent> List2, List<Node> nodes){
+        
+        int xCounter = 0;
+        int yCounter = 0;
+
+        for (Node node : nodes) {
+            if(node.getXDiff()==0){
+                list1.add(xCounter, getMissedEvent());
+            } else{
+                xCounter ++;
+            }
+            if(node.getYDiff()==0){
+                list1.add(yCounter, getMissedEvent());
+            } else{
+                yCounter ++;
+            }
+
+        }
+    }
+
+    private static ReportedEvent getMissedEvent(){
+        return new ReportedEvent("missed", 0, 0);
+    }
+
+    private static List<ReportedEvent> MergeEventLists(List<ReportedEvent> list1, List<ReportedEvent> list2){
+        List<ReportedEvent> finalReport = new ArrayList<>();
+        for (int i = 0; i < list1.size(); i ++) {
+            ReportedEvent event1 = list1.get(i);
+            ReportedEvent event2 = list2.get(i);
+            if(event1.getName().equals(event2.getName())){
+                finalReport.add(
+                    new ReportedEvent(event1.getName(), 
+                    ( event1.getStartedTime() + event2.getStartedTime() ) / 2, 
+                    (event1.getDuration() + event2.getDuration() ) / 2
+                ));
+            }
+            
+        }
         
         return null;
     }
